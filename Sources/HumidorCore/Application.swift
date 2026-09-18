9baa1f203b37a6d013da436dcb3111adb5bf8056
Ivan Eresko <jesterhead2017@gmail.com>
2026-09-18 12:50:49 +1200

    Release 1.2.1

--- Sources/HumidorCore/Application.swift
+++ Sources/HumidorCore/Application.swift
@@ -4,7 +4,7 @@
 public enum Application {
     public static let name = "Humidor"
     public static let identifier = "org.humidor.Humidor"
-    public static let version = "1.2.0"
+    public static let version = "1.2.1"
 
     /// Address returning the latest released version, as the "tag_name" of a GitHub release.
     /// No version check is made while this is empty.
